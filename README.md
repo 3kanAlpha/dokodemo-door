e501ec57baf8c77db2107c286937f52f98f17540
m4gnett <39447635+3kanAlpha@users.noreply.github.com>
2022-04-18 22:02:33 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 遠征どこでもドアもどき (Unofficial port to 1.12.x)
+This mod is originally made by つてと.
---
+++ src/main/java/net/mgcup/dkdmdoor/DokodemoDoorMod.java
@@ -0,0 +1,31 @@
+package net.mgcup.dkdmdoor;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = DokodemoDoorMod.MODID, name = DokodemoDoorMod.NAME, version = DokodemoDoorMod.VERSION)
+public class DokodemoDoorMod
+{
+    public static final String MODID = "dkdmdoor";
+    public static final String NAME = "Dokodemo Door";
+    public static final String VERSION = "0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
